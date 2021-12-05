6f19d09943e69ca38c52d75c72f3105b143e6410
yejukim <yejukim@uw.edu>
2021-12-05 14:18:43 -0800

    merge

--- fatal_encounters/main.js
+++ fatal_encounters/main.js
@@ -39,58 +39,59 @@
 // // If the user change the map (zoom or drag), I update circle position:
 // map.on("moveend", update);
 
-d3.csv("./test.csv").then(function(data){
-    var markers = [];
-    data.forEach(function (d) {
-        d.Latitude = +d.Latitude;
-        d.Longitude = +d.Longitude;
+d3.csv("./test.csv").then(function (data) {
+  var markers = [];
+  data.forEach(function (d) {
+    d.Latitude = +d.Latitude;
+    d.Longitude = +d.Longitude;
 
-        markers.push(([d.Longitude, d.Latitude]))
-    });
-    // setting the map view to the center of the us
-var map = L
-.map('mapid')
-.setView([37.8, -96], 4);   
+    markers.push({ "long": d.Longitude, "lat": d.Latitude })
+  });
 
-// Add a tile to the map = a background. Comes from OpenStreetmap
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-  maxZoom: 6,
+  // setting the map view to the center of the us
+  // setting the map view to the center of the us
+  var map = L
+    .map('mapid')
+    .setView([37.8, -96], 4);
+
+  // Add a tile to the map = a background. Comes from OpenStreetmap
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+    maxZoom: 6,
   }).addTo(map);
 
-// Add a svg layer to the map
-L.svg().addTo(map);
+  // Add a svg layer to the map
+  L.svg().addTo(map);
 
 
-// Select the svg area and add circles:
-d3.select("#mapid")
-.select("svg")
-.selectAll("myCircles")
-.data(markers)
-.enter()
-.append("circle")
-  .attr("cx", function(d){ return map.latLngToLayerPoint([d.Latitude, d.Longitude]).x })
-  .attr("cy", function(d){ return map.latLngToLayerPoint([d.Latitude, d.Longitude]).y })
-  .attr("r", 14)
-  .style("fill", "red")
-  .attr("stroke", "red")
-  .attr("stroke-width", 3)
-  .attr("fill-opacity", .4)
+  // Select the svg area and add circles:
+  d3.select("#mapid")
+    .select("svg")
+    .selectAll("myCircles")
+    .data(markers)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).x })
+    .attr("cy", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).y })
+    .attr("r", 4)
+    .style("fill", "red")
+    .attr("stroke", "red")
+    .attr("stroke-width", .1)
+    .attr("fill-opacity", .2)
 
-// Function that update circle position if something change
-function update() {
-d3.selectAll("circle")
-  .attr("cx", function(d){ return map.latLngToLayerPoint([d.Latitude, d.Longitude]).x })
-  .attr("cy", function(d){ return map.latLngToLayerPoint([d.Latitude, d.Longitude]).y })
-}
+  // Function that update circle position if something change
+  function update() {
+    d3.selectAll("circle")
+      .attr("cx", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).x })
+      .attr("cy", function (d) { return map.latLngToLayerPoint([d.lat, d.long]).y })
+  }
 
-// If the user change the map (zoom or drag), I update circle position:
-map.on("moveend", update);
-})
 
-d3.csv("./fatal_encounters").then(function(dataset){
-  dataset.forEach(function(d){
-    
-  })
-})
+
+  // If the user change the map (zoom or drag), I update circle position:
+  map.on("moveend", update);
+});
+
+
+
